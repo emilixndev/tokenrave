89e9a272f3400a8d0e260e7b0ba334abe2036600
emilixn <emilien.muck@gmail.com>
2025-04-29 15:51:44 +0200

    feat(home): clickable link

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,41 @@
 import {Link, router} from "expo-router";
-import {Pressable, Text, View} from "react-native";
+import {Button, Pressable, Text, View, StyleSheet} from "react-native";
+import {navigate} from "expo-router/build/global-state/routing";
 
 export default function Index() {
-  return (
-      <View>
-
-
-          <Link
-              href={{
-                  pathname: '/events/[id]',
-                  params: { id: '1' }
-              }}>
-              Event 1
-          </Link>
-
-      </View>
-  );
+    return (
+        <View>
+        <View>
+            <Link style={styles.card}
+                  href={{
+                      pathname: '/events/[id]',
+                      params: {id: '1'}
+                  }}>
+                test
+            </Link>
+        </View>
+            <View>
+                <Link style={styles.card}
+                      href={{
+                          pathname: '/events/[id]',
+                          params: {id: '1'}
+                      }}>
+                    test
+                </Link>
+            </View>
+        </View>
+    );
 }
+const styles = StyleSheet.create({
+    card: {
+        borderStyle: "solid",
+        borderColor: "black",
+        borderWidth: 2,
+        textAlign: "center",
+        padding: 15,
+        margin: 15,
+        borderRadius: 10
+
+    },
+
+});
