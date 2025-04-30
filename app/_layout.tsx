66f436e71bec5f4c7164b0a556a49b6841254b5c
emilixn <emilien.muck@gmail.com>
2025-04-30 16:38:15 +0200

    feat(home): Add SQLite integration for events management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,29 @@
-import { Stack } from "expo-router";
+import {Stack} from "expo-router";
+import {SQLiteDatabase, SQLiteProvider} from "expo-sqlite";
 
 export default function RootLayout() {
-  return <Stack   screenOptions={{
-    headerShown: false
-  }} />;
+    const createDB = async (db: SQLiteDatabase) => {
+
+        // TODO REMOVE
+        await db.execAsync(`DROP TABLE events`)
+        await db.execAsync(`CREATE TABLE IF NOT EXISTS events
+                            (
+                                id
+                                INTEGER
+                                PRIMARY
+                                KEY
+                                AUTOINCREMENT,
+                                name
+                                TEXT
+                                NOT
+                                NULL
+                            );`)
+
+    }
+    return (
+        <SQLiteProvider databaseName="tokenrave.db" onInit={createDB}>
+            <Stack screenOptions={{
+                headerShown: false
+            }}/>
+        </SQLiteProvider>);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,44 @@
 import {Link, router} from "expo-router";
 import {Button, Pressable, Text, View, StyleSheet} from "react-native";
 import {navigate} from "expo-router/build/global-state/routing";
+import {SQLiteDatabase, useSQLiteContext} from "expo-sqlite";
+import {useEffect, useState} from "react";
 
 export default function Index() {
+
+    const database = useSQLiteContext()
+
+    const [events, setEvents] = useState<any>([])
+
+    useEffect(() => {
+        database.runSync(`INSERT INTO events (name)
+                          VALUES ('test')`)
+        database.runSync(`INSERT INTO events (name)
+                          VALUES ('test')`)
+        let eventsTest = database.getAllSync(`SELECT *
+                                              FROM events`)
+        console.log(eventsTest)
+        setEvents(eventsTest)
+        console.log(events)
+    }, []);
     return (
         <View>
-        <View>
-            <Link style={styles.card}
-                  href={{
-                      pathname: '/events/[id]',
-                      params: {id: '1'}
-                  }}>
-                test
-            </Link>
-        </View>
-            <View>
-                <Link style={styles.card}
-                      href={{
-                          pathname: '/events/[id]',
-                          params: {id: '1'}
-                      }}>
-                    test
-                </Link>
-            </View>
+
+            {events ? events.map((value: any) => (
+                    <View key={value.id}>
+                        <Link style={styles.card}
+                              href={{
+                                  pathname: '/events/[id]',
+                                  params: {id: value.id}
+                              }}>
+                            <Text>{value.name}</Text>
+                        </Link>
+                    </View>
+
+                ))
+                : <Text>RIEN</Text>};
+
+
         </View>
     );
 }
