6c3aa05262673291fa70d2e6dd4f185b71cafd47
emilixn <emilien.muck@gmail.com>
2025-05-06 14:05:37 +0200

    feat(events): add price input and token input

--- app/events/[id].tsx
+++ app/events/[id].tsx
@@ -10,6 +10,8 @@ export default function Index() {
   const database = useSQLiteContext();
   const [token, setToken] = useState<number>(0);
   const [tokenInput, setTokenInput] = useState<number>(0);
+  const [priceInput, setPriceInput] = useState<number>(0);
+  const [price, setPrice] = useState<number>(0);
 
   const [event, setEvent] = useState<any>([]);
   useEffect(() => {
@@ -31,7 +33,7 @@ export default function Index() {
         width="100%"
         orientation="horizontal"
         flexDirection="column"
-        height={150}
+        height="100%"
         borderRadius="$4"
         borderWidth="$0.25"
         overflow="hidden"
@@ -45,7 +47,7 @@ export default function Index() {
             flex={1}
             value="tab1"
           >
-            <SizableText>Tab 1</SizableText>
+            <SizableText>Tokens</SizableText>
           </Tabs.Tab>
           <Tabs.Tab
             focusStyle={{
@@ -54,13 +56,14 @@ export default function Index() {
             flex={1}
             value="tab2"
           >
-            <SizableText>Tab 2</SizableText>
+            <SizableText>History</SizableText>
           </Tabs.Tab>
         </Tabs.List>
 
         <Tabs.Content value="tab1">
-          <Text>xx€</Text>
-          <Text>{ token } Tokens</Text>
+          <Text>{price} €</Text>
+          <Text>{token} Tokens</Text>
+          <Text>{price/token}€ = 1 Token</Text>
           <Text>Event {event.name}</Text>
           <Text>Token</Text>
           <Dialog modal>
@@ -110,19 +113,35 @@ export default function Index() {
                   <Label justifyContent="flex-end" htmlFor="name">
                     Number of token :
                   </Label>
-                  <Input flex={1} id="number_token" placeholder="xx" onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {setTokenInput(parseInt(e.nativeEvent.text))}} />
+                  <Input
+                    flex={1}
+                    id="number_token"
+                    placeholder="xx"
+                    onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+                      setTokenInput(parseInt(e.nativeEvent.text));
+                    }}
+                  />
                 </Fieldset>
 
                 <Fieldset gap="$4" horizontal>
                   <Label justifyContent="flex-end" htmlFor="name">
                     Price in € :
                   </Label>
-                  <Input flex={1} id="price_token" placeholder="xx,xx" />
+                  <Input flex={1} id="price_token" placeholder="xx,xx"   onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+                    setPriceInput(parseFloat(e.nativeEvent.text));
+                  }} />
                 </Fieldset>
 
                 <XStack alignSelf="flex-end" gap="$4">
                   <Dialog.Close displayWhenAdapted asChild>
-                    <Button theme="accent" aria-label="Close" onPress={() => {setToken(token + tokenInput)}}>
+                    <Button
+                      theme="accent"
+                      aria-label="Close"
+                      onPress={() => {
+                        setToken(token + tokenInput);
+                        setPrice(price + priceInput);
+                      }}
+                    >
                       Save changes
                     </Button>
                   </Dialog.Close>
@@ -136,8 +155,96 @@ export default function Index() {
               </Dialog.Content>
             </Dialog.Portal>
           </Dialog>
-          <Button>Add Tokens</Button>
-          <Button>Add expense</Button>
+
+          <Dialog modal>
+            <Dialog.Trigger asChild>
+              <Button>Add expense</Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay
+                key="overlay"
+                backgroundColor="$shadow6"
+                animateOnly={['transform', 'opacity']}
+                animation={[
+                  'quicker',
+                  {
+                    opacity: {
+                      overshootClamping: true,
+                    },
+                  },
+                ]}
+                enterStyle={{ opacity: 0, scale: 0.95 }}
+                exitStyle={{ opacity: 0 }}
+              />
+
+              <Dialog.Content
+                bordered
+                width="95%"
+                height={'auto'}
+                elevate
+                key="content"
+                animateOnly={['transform', 'opacity']}
+                animation={[
+                  'quicker',
+                  {
+                    opacity: {
+                      overshootClamping: true,
+                    },
+                  },
+                ]}
+                enterStyle={{ x: 0, y: -20, opacity: 0 }}
+                exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+                gap="$4"
+              >
+                <Dialog.Title>Add token</Dialog.Title>
+                <Dialog.Description>Add token to your wallet</Dialog.Description>
+                <Fieldset gap="$4" horizontal>
+                  <Label justifyContent="flex-end" htmlFor="name">
+                    Number of token :
+                  </Label>
+                  <Input
+                    flex={1}
+                    id="number_token"
+                    placeholder="xx"
+                    onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+                      setTokenInput(parseInt(e.nativeEvent.text));
+                    }}
+                  />
+                </Fieldset>
+
+                <Fieldset gap="$4" horizontal>
+                  <Label justifyContent="flex-end" htmlFor="name">
+                    Price in € :
+                  </Label>
+                  <Input flex={1} id="price_token" placeholder="xx,xx"   onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+                    setPriceInput(parseFloat(e.nativeEvent.text));
+                  }} />
+                </Fieldset>
+
+                <XStack alignSelf="flex-end" gap="$4">
+                  <Dialog.Close displayWhenAdapted asChild>
+                    <Button
+                      theme="accent"
+                      aria-label="Close"
+                      onPress={() => {
+                        setToken(token + tokenInput);
+                        setPrice(price + priceInput);
+                      }}
+                    >
+                      Save changes
+                    </Button>
+                  </Dialog.Close>
+                </XStack>
+
+                <Unspaced>
+                  <Dialog.Close asChild>
+                    <Button position="absolute" top="$3" right="$3" size="$2" circular icon={X} />
+                  </Dialog.Close>
+                </Unspaced>
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog>
         </Tabs.Content>
         <Tabs.Content value="tab2">
           <H5>Tab 2</H5>
